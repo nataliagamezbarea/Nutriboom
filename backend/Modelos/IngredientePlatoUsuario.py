8df1edbaee83936b6bcc451299b0316ef028bcb6
Aduartel-SJO <aduartel@alumnes.santjosepobrer.es>
2025-04-01 21:25:29 +0200

    error en modelo

--- backend/Modelos/IngredientePlatoUsuario.py
+++ backend/Modelos/IngredientePlatoUsuario.py
@@ -5,4 +5,4 @@ class IngredientePlato(db.Model):
     id_ingrediente = db.Column(db.Integer, primary_key=True)
     id_usuario = db.Column(db.Integer, primary_key=True)
     cantidad = db.Column(db.Decimal(6, 2), nullable=False)
-    day = db.Column(db.Enum('Lunes','Martes','Miercoles','Jueves','Viernes','Sabado','Domingo'))
+    dia = db.Column(db.Enum('Lunes','Martes','Miercoles','Jueves','Viernes','Sabado','Domingo'))
