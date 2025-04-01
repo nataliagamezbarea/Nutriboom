d1135d027ac3762a41f7a7c36af60fcbd47cdaa7
Aduartel-SJO <aduartel@alumnes.santjosepobrer.es>
2025-04-01 20:13:50 +0200

    EliminandoCodigoInecesario

--- app.py
+++ app.py
@@ -17,21 +17,6 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
 
-class Usuario(db.Model): 
-    id_usuario = db.Column(db.Integer, primary_key=True)
-    nombre = db.Column(db.String(50), nullable=False)
-    apellido1 = db.Column(db.String(50), nullable=True)
-    apellido2 = db.Column(db.String(50), nullable=True)
-    email = db.Column(db.String(50), nullable=False)
-    contrasena =  db.Column(db.String(100), nullable=False)
-    is_admin = db.Column(db.Boolean, nullable=False)
-
-
-with app.app_context():
-    usuarios = Usuario.query.all()
-    for usuario in usuarios:
-        print(usuario.id_usuario, usuario.nombre, usuario.email)
-        
 @app.route('/')
 def home():
     return render_template("SeguimientoNutricional.html", )
