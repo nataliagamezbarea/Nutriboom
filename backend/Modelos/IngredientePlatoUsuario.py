0a6b8080c49a6b7164d70ba259f35526fa504eb3
nataliagamezbarea <144036430+nataliagamezbarea@users.noreply.github.com>
2025-04-14 19:03:09 +0200

    Update IngredientePlatoUsuario.py
    Cambiar atributo de day por dia

--- backend/Modelos/IngredientePlatoUsuario.py
+++ backend/Modelos/IngredientePlatoUsuario.py
@@ -6,4 +6,4 @@ class IngredientePlatoUsuario(db.Model):
     id_ingrediente = db.Column(db.Integer, primary_key=True)
     id_usuario = db.Column(db.Integer, primary_key=True)
     cantidad = db.Column(db.Numeric(6, 2), nullable=False)
-    day = db.Column(db.Enum('Lunes','Martes','Miercoles','Jueves','Viernes','Sabado','Domingo'))
+    dia = db.Column(db.Enum('Lunes','Martes','Miercoles','Jueves','Viernes','Sabado','Domingo'))
